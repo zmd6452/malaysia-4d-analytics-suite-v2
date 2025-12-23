83b7c8c6f1a8a15f722211b6d99086326fbd4bda
Zul <rukia6452@gmail.com>
2025-12-23 12:41:08 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIG = {
+  mustIncludeDigits: [1, 2],     // 🔥 REQUIRED digits
+  excludedDigits: [],            // optional
+  totalPicks: 6,                 // how many numbers
+  digitLength: 4,                // 4D
+
+  digitWeights: {
+    0: 1,
+    1: 3,
+    2: 3,
+    3: 1,
+    4: 1,
+    5: 1,
+    6: 1,
+    7: 1,
+    8: 1,
+    9: 1
+  }
+};
